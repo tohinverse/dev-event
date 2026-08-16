04e002e5f8e47ce155a75325f9e5699bb4ff8c4b
Tohin <tohin@linno.io>
2026-08-16 19:03:49 +0600

    Add events API route for creating and listing events

---
+++ app/api/events/route.ts
@@ -0,0 +1,39 @@
+import {NextRequest, NextResponse} from "next/server";
+
+import connectDB from "@/lib/mongodb";
+import { Event } from '@/database/event.model';
+
+export async function POST(req: NextRequest) {
+    try {
+        await connectDB();
+
+        const formData = await req.formData();
+
+        let event;
+
+        try {
+            event = Object.fromEntries(formData.entries());
+        } catch (e) {
+            return NextResponse.json({ message: 'Invalid JSON data format'}, { status: 400 })
+        }
+
+        const createdEvent = await Event.create(event);
+
+        return NextResponse.json({ message: 'Event created successfully', event: createdEvent }, { status: 201 });
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json({ message: 'Event Creation Failed', error: e instanceof Error ? e.message : 'Unknown'}, { status: 500 })
+    }
+}
+
+export async function GET() {
+    try {
+        await connectDB();
+
+        const events = await Event.find().sort({ createdAt: -1 });
+
+        return NextResponse.json({ message: 'Events fetched successfully', events }, { status: 200 });
+    } catch (e) {
+        return NextResponse.json({ message: 'Event fetching failed', error: e }, { status: 500 });
+    }
+}
